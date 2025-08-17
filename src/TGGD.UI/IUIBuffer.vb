147b54ccec1f7af72d6cc6ec41dda1c705abe53d
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-08-17 05:58:22 -0500

    got a title screen

---
+++ src/TGGD.UI/IUIBuffer.vb
@@ -0,0 +1,7 @@
+﻿Public Interface IUIBuffer(Of TPixel)
+    Sub SetPixel(column As Integer, row As Integer, hue As TPixel)
+    Sub Fill(column As Integer, row As Integer, columns As Integer, rows As Integer, hue As TPixel)
+    Sub Fill(hue As TPixel)
+    ReadOnly Property Columns As Integer
+    ReadOnly Property Rows As Integer
+End Interface
---
+++ src/TGGDGGJ2.UI/TitleState.vb
@@ -0,0 +1,27 @@
+﻿Imports TGGD.UI
+Imports TGGDGGJ2.Business
+
+Friend Class TitleState
+    Implements IUIState
+
+    Private ReadOnly buffer As IUIBuffer(Of Integer)
+    Private ReadOnly world As IWorld
+    Private ReadOnly playSfx As Action(Of String)
+
+    Public Sub New(buffer As IUIBuffer(Of Integer), world As IWorld, playSfx As Action(Of String))
+        Me.buffer = buffer
+        Me.world = world
+        Me.playSfx = playSfx
+    End Sub
+
+    Public Sub Refresh() Implements IUIState.Refresh
+        buffer.Fill(239)
+        buffer.WriteText((4, 1), "ADELINE'S  SQUISHMALLOWS")
+        buffer.WriteText((0, 3), "A PRODUCTION OF THEGRUMPYGAMEDEV")
+        buffer.WriteText((4, 4), "FOR GIRLY GAME JAM #2")
+    End Sub
+
+    Public Function HandleCommand(command As String) As IUIState Implements IUIState.HandleCommand
+        Return Me
+    End Function
+End Class
